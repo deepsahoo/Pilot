d6f1d0c2721e4016b82d60ee516784b1cb90bf58
deepsahoo <deepsahoo@USBLRDEEPSAHOO1.us.deloitte.com>
2018-01-25 16:55:09 +0530

    first commit

---
+++ src/main/java/com/example/demo/SpringBootPilotApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPilotApplication.class, args);
+	}
+}
